e0589dc67cef16402af28d732109f3954715a58f
MatheusSuzart <mathsuzart@outlook.com>
2020-11-29 23:18:28 -0300

    second

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/home',function(req,res){
-    res.send("<html><body><h1>Hello World</h1></body></html>");
+app.set('view engine','ejs');
+
+app.get('/produtos',function(req,res){
+    res.render("produtos/lista");
 });
 
 app.listen(3000,function(){
